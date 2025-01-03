3e5d349642ca2c03404408ae1c29fcf59b07af81
Kristupas Antanavičius <kristupas.antanavicius@nordsec.com>
2025-01-03 18:12:03 +0200

    Debug CI

--- fixtures/ext-types/lib/tests/bindings/test_imported_types.swift
+++ fixtures/ext-types/lib/tests/bindings/test_imported_types.swift
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+print("AAAAAAAAAAAAA test_imported_types.swift")
+
+assert(false)
+
 import imported_types_lib
 import Foundation
 
