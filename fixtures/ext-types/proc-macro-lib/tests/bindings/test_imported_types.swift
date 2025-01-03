7e8b77fcaafb005ed719f412d3121d6fcd738204
Kristupas Antanavičius <kristupas.antanavicius@nordsec.com>
2025-01-03 18:23:44 +0200

    Debug CI

--- fixtures/ext-types/proc-macro-lib/tests/bindings/test_imported_types.swift
+++ fixtures/ext-types/proc-macro-lib/tests/bindings/test_imported_types.swift
@@ -1,59 +1,79 @@
-// /* This Source Code Form is subject to the terms of the Mozilla Public
-//  * License, v. 2.0. If a copy of the MPL was not distributed with this
-//  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// import imported_types_lib
-// import Foundation
+print("AAAAAAAAAAAAAAAAAAAAAAA 1")
 
-// let ct = getCombinedType(value: nil)
-// assert(ct.uot.sval == "hello")
-// assert(ct.guid ==  "a-guid")
-// assert(ct.url ==  URL(string: "http://example.com/"))
+import imported_types_lib
+import Foundation
 
-// let ct2 = getCombinedType(value: ct)
-// assert(ct == ct2)
+print("AAAAAAAAAAAAAAAAAAAAAAA 2")
 
-// assert(getObjectsType(value: nil).maybeInterface == nil)
-// assert(getObjectsType(value: nil).maybeTrait == nil)
-// assert(getUniffiOneTrait(t: nil) == nil)
+let ct = getCombinedType(value: nil)
+assert(ct.uot.sval == "hello")
+assert(ct.guid ==  "a-guid")
+assert(ct.url ==  URL(string: "http://example.com/"))
 
-// let url = URL(string: "http://example.com/")!;
-// assert(getUrl(url: url) ==  url)
-// assert(getMaybeUrl(url: url)! == url)
-// assert(getMaybeUrl(url: nil) == nil)
-// assert(getUrls(urls: [url]) == [url])
-// assert(getMaybeUrls(urls: [url, nil]) == [url, nil])
+print("AAAAAAAAAAAAAAAAAAAAAAA 3")
 
-// assert(getUniffiOneType(t: UniffiOneType(sval: "hello")).sval == "hello")
-// assert(getMaybeUniffiOneType(t: UniffiOneType(sval: "hello"))!.sval == "hello")
-// assert(getMaybeUniffiOneType(t: nil) == nil)
-// assert(getUniffiOneTypes(ts: [UniffiOneType(sval: "hello")]) == [UniffiOneType(sval: "hello")])
-// assert(getMaybeUniffiOneTypes(ts: [UniffiOneType(sval: "hello"), nil]) == [UniffiOneType(sval: "hello"), nil])
+let ct2 = getCombinedType(value: ct)
+assert(ct == ct2)
 
-// var counter = DispatchGroup()
-// counter.enter()
-// Task {
-//     // This async function comes from the `uniffi-one` crate
-//     let uniffiOneEnum = await getUniffiOneAsync()
-//     assert(uniffiOneEnum == UniffiOneEnum.one)
+assert(getObjectsType(value: nil).maybeInterface == nil)
+assert(getObjectsType(value: nil).maybeTrait == nil)
+assert(getUniffiOneTrait(t: nil) == nil)
 
-//     // This async function comes from the `proc-macro-lib` crate
-//     let uniffiOneType = await getUniffiOneTypeAsync(t: UniffiOneType(sval: "hello"))
-//     assert(uniffiOneType.sval == "hello")
+print("AAAAAAAAAAAAAAAAAAAAAAA 4")
 
-//     counter.leave()
-// }
-// counter.wait()
+let url = URL(string: "http://example.com/")!;
+assert(getUrl(url: url) ==  url)
+assert(getMaybeUrl(url: url)! == url)
+assert(getMaybeUrl(url: nil) == nil)
+assert(getUrls(urls: [url]) == [url])
+assert(getMaybeUrls(urls: [url, nil]) == [url, nil])
+
+print("AAAAAAAAAAAAAAAAAAAAAAA 5")
+
+assert(getUniffiOneType(t: UniffiOneType(sval: "hello")).sval == "hello")
+assert(getMaybeUniffiOneType(t: UniffiOneType(sval: "hello"))!.sval == "hello")
+assert(getMaybeUniffiOneType(t: nil) == nil)
+assert(getUniffiOneTypes(ts: [UniffiOneType(sval: "hello")]) == [UniffiOneType(sval: "hello")])
+assert(getMaybeUniffiOneTypes(ts: [UniffiOneType(sval: "hello"), nil]) == [UniffiOneType(sval: "hello"), nil])
+
+print("AAAAAAAAAAAAAAAAAAAAAAA 6")
+
+var counter = DispatchGroup()
+counter.enter()
+Task {
+    // This async function comes from the `uniffi-one` crate
+    let uniffiOneEnum = await getUniffiOneAsync()
+    assert(uniffiOneEnum == UniffiOneEnum.one)
+
+    // This async function comes from the `proc-macro-lib` crate
+    let uniffiOneType = await getUniffiOneTypeAsync(t: UniffiOneType(sval: "hello"))
+    assert(uniffiOneType.sval == "hello")
+
+    counter.leave()
+}
+counter.wait()
+
+print("AAAAAAAAAAAAAAAAAAAAAAA 7")
+
+assert(getUniffiOneProcMacroType(t: UniffiOneProcMacroType(sval: "hello from proc-macro world")).sval == "hello from proc-macro world")
+assert(getMyProcMacroType(t: UniffiOneProcMacroType(sval: "proc-macros all the way down")).sval == "proc-macros all the way down")
 
 
-// assert(getUniffiOneProcMacroType(t: UniffiOneProcMacroType(sval: "hello from proc-macro world")).sval == "hello from proc-macro world")
-// assert(getMyProcMacroType(t: UniffiOneProcMacroType(sval: "proc-macros all the way down")).sval == "proc-macros all the way down")
+print("AAAAAAAAAAAAAAAAAAAAAAA 8")
 
-// assert(getUniffiOneEnum(e: UniffiOneEnum.one) == UniffiOneEnum.one)
-// assert(getMaybeUniffiOneEnum(e: UniffiOneEnum.one)! == UniffiOneEnum.one)
-// assert(getMaybeUniffiOneEnum(e: nil) == nil)
-// assert(getUniffiOneEnums(es: [UniffiOneEnum.one]) == [UniffiOneEnum.one])
-// assert(getMaybeUniffiOneEnums(es: [UniffiOneEnum.one, nil]) == [UniffiOneEnum.one, nil])
+assert(getUniffiOneEnum(e: UniffiOneEnum.one) == UniffiOneEnum.one)
+assert(getMaybeUniffiOneEnum(e: UniffiOneEnum.one)! == UniffiOneEnum.one)
+assert(getMaybeUniffiOneEnum(e: nil) == nil)
+assert(getUniffiOneEnums(es: [UniffiOneEnum.one]) == [UniffiOneEnum.one])
+assert(getMaybeUniffiOneEnums(es: [UniffiOneEnum.one, nil]) == [UniffiOneEnum.one, nil])
 
-// let g = getGuidProcmacro(g: nil)
-// assert(g == getGuidProcmacro(g: g))
+print("AAAAAAAAAAAAAAAAAAAAAAA 9")
+
+let g = getGuidProcmacro(g: nil)
+assert(g == getGuidProcmacro(g: g))
+
+print("AAAAAAAAAAAAAAAAAAAAAAA 10")
